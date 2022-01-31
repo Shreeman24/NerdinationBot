114edfd36017d98dc1b98af02916df5a97d1f560
Shreeman <shreeman24mc@gmail.com>
2022-01-30 22:07:00 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from os import getenv
+from flask import Flask, request
+from gmbot import GroupMeBot
+from lfbot import LFBot
+from sabot import SABot
+from rabot import RABot
+
+
+app = Flask(__name__)
+
+
+test_bot = RABot(getenv("TEST_BOT_ID"), getenv("TEST_BOT_NAME"), getenv("TEST_GROUP_ID"))
+@app.route("/test", methods=["POST"])
+def read_test_group():
+    data = request.get_json()
+    test_bot.chat(data)
+    return "OK", 200
+
+    
+
+
+lf_bot = LFBot(getenv("36b1299bb959a0c08f1d51a157"), getenv("Nerdination Bot"), getenv("69888400"))
+@app.route("/lf", methods=["POST"])
+def read_lf_group():
+    data = request.get_json()
+    lf_bot.chat(data)
+    return "OK", 200
+
+
+sa_bot = SABot(getenv("SA_BOT_ID"), getenv("SA_BOT_NAME"), getenv("SA_GROUP_ID"))
+@app.route("/sa", methods=["POST"])
+def read_sa_group():
+    data = request.get_json()
+    sa_bot.chat(data)
+    return "OK", 200
+
+
+ra_bot = RABot(getenv("RA_BOT_ID"), getenv("RA_BOT_NAME"), getenv("RA_GROUP_ID"))
+@app.route("/ra", methods=["POST"])
+def read_ra_group():
+    data = request.get_json()
+    ra_bot.chat(data)
+    return "OK", 200
